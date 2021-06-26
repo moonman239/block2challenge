8deede51ac3a55fd52dec2590aa2441e002c89c1
Montana Burr <bur18019@byui.edu>
2021-06-26 13:41:21 -0600

    Added test to sort by ascending distance.

--- index.js
+++ index.js
@@ -23,9 +23,28 @@ async function test2()
         const rating = parseFloat(restaurant.rating);
         if (previousRating > rating)
             throw new Error("Test 2 failed.")
+        previousRating = rating;
     }
     console.log("Test 2 passed.");
 }
 
+// Test sorting by ascending distance.
+async function test3()
+{
+    const restaurantList = await RestaurantList.fetchFromLatitudeLongitude(testLatitude,testLongitude);
+    restaurantList.sort("distance");
+    let previousDistance = -123456;
+    for (let restaurant of restaurantList.generator())
+    {
+        const distance = parseFloat(restaurant.distance);
+        if (previousDistance === -123456)
+            previousDistance = distance;
+        if (distance < previousDistance)
+            throw new Error("Test 3 failed.");
+        previousDistance = distance;
+    }
+    console.log("Test 3 passed.");
+}
 test1();
 test2();
+test3();
