e07cacf36f7c2becf8a0c1ac47e4d13a912f3da3
Montana Burr <bur18019@byui.edu>
2021-06-26 13:19:46 -0600

    Structured in the form of a test.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const testLatitude = 39.14659888817281;
 const testLongitude = -108.64285062918833;
 import {RestaurantList} from './RestaurantList.js';
+
+// Test to ensure successful fetch.
 async function test1()
 {
     const restaurantList = await RestaurantList.fetchFromLatitudeLongitude(testLatitude,testLongitude);
-    console.log(restaurantList.generator().next());
-}
+    const next = restaurantList.generator().next();
+    if (!next)
+        throw new Error("Undefined location_id.");
+    console.log("Test 1 passed.");
+    }
 
 test1();
