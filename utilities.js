1746ba19b8f7eec617355fa7c442892ec77eb735
Montana Burr <bur18019@byui.edu>
2021-06-30 12:41:53 -0600

    Changed indentation.

--- utilities.js
+++ utilities.js
@@ -16,18 +16,18 @@ export async function getLocation()
      window.userPosition = position;
             }
 
-            export async function geocodeAddress(address)
-            {
-                let response = await fetch("https://forward-reverse-geocoding.p.rapidapi.com/v1/search?q=" + address + "%20USA&format=json&accept-language=en&polygon_threshold=0.0", {
-                    "method": "GET",
-                    "headers": {
-                        "x-rapidapi-key": "1167886aeemsh061eed0f807e535p17f6aajsnc70cd0d6bdaa",
-                        "x-rapidapi-host": "forward-reverse-geocoding.p.rapidapi.com"
+export async function geocodeAddress(address)
+    {
+        let response = await fetch("https://forward-reverse-geocoding.p.rapidapi.com/v1/search?q=" + address + "%20USA&format=json&accept-language=en&polygon_threshold=0.0", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "1167886aeemsh061eed0f807e535p17f6aajsnc70cd0d6bdaa",
+                "x-rapidapi-host": "forward-reverse-geocoding.p.rapidapi.com"
                     }
                 })
-                let json = await response.json();
-                const userPosition = {coords : {latitude: json[0].lat, longitude: json[0].lon}};
-                if (userPosition.coords.latitude === undefined || userPosition.coords.longitude === undefined)
-                    throw new Error("Undefined latitude and longitude.");
-                window.userPosition = userPosition;
-            }
+            let json = await response.json();
+            const userPosition = {coords : {latitude: json[0].lat, longitude: json[0].lon}};
+            if (userPosition.coords.latitude === undefined || userPosition.coords.longitude === undefined)
+                throw new Error("Undefined latitude and longitude.");
+            window.userPosition = userPosition;
+        }
